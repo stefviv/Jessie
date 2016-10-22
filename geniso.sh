da2ce5c9e1ab3cf0b22566cbd2141fe99a8947cd
stefviv <tech@DEBIAN>
2016-10-22 20:14:48 +0200

    MAJ

--- geniso.sh
+++ geniso.sh
@@ -16,16 +16,17 @@
 #===========================================================
 #!/bin/bash
 DATE=_`date "+%Y%m%d-%Hh%M"`
+VER=8.6.0
 apt-get update -y && apt-get upgrade -y
 clear
 apt-get install wget unzip bsdtar genisoimage git -y
 sleep 30
 cd /root
-wget http://cdimage.debian.org/debian-cd/8.1.0/amd64/iso-cd/debian-8.1.0-amd64-CD-1.iso
-mkdir /root/debian-8.1.0-amd64-preseed
+wget http://cdimage.debian.org/debian-cd/"$VER"/amd64/iso-cd/debian-"$VER"-amd64-CD-1.iso
+mkdir /root/debian-"$VER"-amd64-preseed
 mkdir /root/initrd-preseed
-bsdtar -C debian-8.1.0-amd64-preseed/ -xf debian-8.1.0-amd64-CD-1.iso
-mv debian-8.1.0-amd64-preseed/install.amd/initrd.gz initrd-preseed/
+bsdtar -C debian-"$VER"-amd64-preseed/ -xf debian-"$VER"-amd64-CD-1.iso
+mv debian-"$VER"-amd64-preseed/install.amd/initrd.gz initrd-preseed/
 cd /root/initrd-preseed/
 gunzip -c initrd.gz | cpio -id
 rm /root/initrd-preseed/initrd.gz
@@ -34,9 +35,9 @@ git clone https://github.com/stefviv/Jessie;
 cp /root/Jessie/preseed.cfg /root/initrd-preseed/
 chmod 755 /root/initrd-preseed/preseed.cfg
 cd /root/initrd-preseed/
-find . | cpio -H newc --create --verbose | gzip -9 > /root/debian-8.1.0-amd64-preseed/install.amd/initrd.gz
-cd /root/debian-8.1.0-amd64-preseed/
+find . | cpio -H newc --create --verbose | gzip -9 > /root/debian-"$VER"-amd64-preseed/install.amd/initrd.gz
+cd /root/debian-"$VER"-amd64-preseed/
 ### lrwxrwxrwx  1 root root 1 fevr.  8 14:46 debian -> .
-unlink /root/debian-8.1.0-amd64-preseed/debian
-genisoimage -o ../debian-8.1.0-amd64-preseed"$DATE".iso -r -J -no-emul-boot -boot-load-size 4 -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat .
-ls -lh /root/debian-8.1.0-amd64-preseed*.iso
+unlink /root/debian-"$VER"-amd64-preseed/debian
+genisoimage -o ../debian-"$VER"-amd64-preseed"$DATE".iso -r -J -no-emul-boot -boot-load-size 4 -boot-info-table -b isolinux/isolinux.bin -c isolinux/boot.cat .
+ls -lh /root/debian-"$VER"-amd64-preseed*.iso
